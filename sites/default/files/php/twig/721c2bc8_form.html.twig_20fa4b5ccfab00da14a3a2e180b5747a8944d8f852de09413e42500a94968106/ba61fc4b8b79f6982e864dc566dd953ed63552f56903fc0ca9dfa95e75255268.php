b04f6c323572824a6b40f2d73b71f95fe85ae569
Jun Zheng <junzheng@debian>
2016-02-09 14:42:28 +0800

    Ini Drupal 8

---
+++ sites/default/files/php/twig/721c2bc8_form.html.twig_20fa4b5ccfab00da14a3a2e180b5747a8944d8f852de09413e42500a94968106/ba61fc4b8b79f6982e864dc566dd953ed63552f56903fc0ca9dfa95e75255268.php
@@ -0,0 +1,60 @@
+<?php
+
+/* core/themes/classy/templates/form/form.html.twig */
+class __TwigTemplate_9ba8030c0dc42dfe50977782aee20fd4f4b5df33c6e39500e0ac014db2ed1e6b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 13
+        echo "<form";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true);
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true);
+        echo "
+</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  24 => 14,  19 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a 'form' element.*/
+/*  **/
+/*  * Available variables*/
+/*  * - attributes: A list of HTML attributes for the wrapper element.*/
+/*  * - children: The child elements of the form.*/
+/*  **/
+/*  * @see template_preprocess_form()*/
+/*  *//* */
+/* #}*/
+/* <form{{ attributes }}>*/
+/*   {{ children }}*/
+/* </form>*/
+/* */
---
+++ sites/default/files/php/twig/721c2bc8_menu-local-action.html.twig_c878136770a58ab780d3d09eb766ab1994a9620725b24dec9ed81db5a7252048/aac8a7f2cfb801c81adc3867d3197d551760ff35b7c5a01d5f0eae567769009c.php
@@ -0,0 +1,55 @@
+<?php
+
+/* core/themes/classy/templates/navigation/menu-local-action.html.twig */
+class __TwigTemplate_faa3792cff803207140fda7a76cf9bc508fb78c579fdd9e98ea1258e9c035147 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 13
+        echo "<li";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true);
+        echo ">";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["link"]) ? $context["link"] : null), "html", null, true);
+        echo "</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/navigation/menu-local-action.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a single local action link.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: HTML attributes for the wrapper element.*/
+/*  * - link: A rendered link element.*/
+/*  **/
+/*  * @see template_preprocess_menu_local_action()*/
+/*  *//* */
+/* #}*/
+/* <li{{ attributes }}>{{ link }}</li>*/
+/* */
